3f230f7deb346ccd5e62851e42298ab40cc0a51c
merrelltd72 <tyron.merrell@gmail.com>
2024-08-06 12:04:45 -0500

    Added User, Room, RoomImage, Reservation and Review models.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # this is stomething new added by Mike
   # Additional comment added by Tyron
+
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
 end
