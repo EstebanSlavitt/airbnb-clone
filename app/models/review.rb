24ad9f07e0da81697efbd5ab4d22de06d2b71dcc
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-19 22:18:36 -0400

    Built out the models' relationships.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ApplicationRecord
+  belongs_to :reservation
+  belongs_to :user, through: :reservation
+  belongs_to :room, through: :reservation
+  
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
 
-  has_many :rooms
+  has_many :owned_rooms, class_name: 'Room', foreign_key: 'user_id'
+  has_many :reservations
+  has_many :reserved_rooms, through: :reservations, source: :room
+  has_many :reviews
 end
